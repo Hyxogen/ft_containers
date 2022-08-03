d31b28b9240111262be69e7e5d5e9cc73aec924a
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-08-03 16:08:52 +0200

    format classes.hpp

--- test/support/classes.hpp
+++ test/support/classes.hpp
@@ -17,53 +17,48 @@
 #ifndef CLASSES_HPP
 #define CLASSES_HPP
 
-#include <string>
 #include <stdexcept>
+#include <string>
 
 namespace test {
 
 struct too_many_instantiations : public std::logic_error {
-  too_many_instantiations(const std::string &what) : logic_error(what) {}
-};
-  
-template<int ThrowAfter>
-class throwing_class {
-  static int _left;
-
-public:
-    throwing_class() {
-    check_and_decrease_counter();
-  }
-  
-  throwing_class(const throwing_class &other) {
-    (void) other;
-    check_and_decrease_counter();
-  };
-  
-  ~throwing_class() {}
-
-  static void reset_counter() {
-    _left = ThrowAfter;
-  }
-
-  throwing_class &operator=(const throwing_class &other) {
-    if (this != &other) {
-      check_and_decrease_counter();
-    }
-    return *this;
-  }
-
-private:
-  static void check_and_decrease_counter() {
-    if (_left == 0) {
-      throw too_many_instantiations("too many instantions of type");
-    }
-    --_left;
- }
+        too_many_instantiations(const std::string &what) : logic_error(what) {}
 };
 
-template<int ThrowAfter>
-int throwing_class<ThrowAfter>::_left = ThrowAfter;
+template <int ThrowAfter> class throwing_class {
+        static int _left;
+
+      public:
+        throwing_class() { check_and_decrease_counter(); }
+
+        throwing_class(const throwing_class &other) {
+                (void)other;
+                check_and_decrease_counter();
+        };
+
+        ~throwing_class() {}
+
+        static void reset_counter() { _left = ThrowAfter; }
+
+        throwing_class &operator=(const throwing_class &other) {
+                if (this != &other) {
+                        check_and_decrease_counter();
+                }
+                return *this;
+        }
+
+      private:
+        static void check_and_decrease_counter() {
+                if (_left == 0) {
+                        throw too_many_instantiations(
+                            "too many instantions of type");
+                }
+                --_left;
+        }
+};
+
+template <int ThrowAfter> int throwing_class<ThrowAfter>::_left = ThrowAfter;
 }
 
 #endif /* CLASSES_HPP */
