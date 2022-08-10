4de06bfd751e802537d4da5d236610d2d55f19d7
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-08-10 14:35:05 +0200

    add iterator traits

---
+++ include/__iterator/iterator_traits.hpp
@@ -0,0 +1,50 @@
+// custom c++ iterator traits implementation
+
+// Copyright (C) 2022 Daan Meijer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#ifndef ITERATOR_TRAITS_HPP
+#define ITERATOR_TRAITS_HPP
+
+#include <cstddef>
+#include <iterator>
+
+namespace ft {
+
+template <class Iterator> struct iterator_traits {
+        typedef typename Iterator::difference_type difference_type;
+        typedef typename Iterator::value_type value_type;
+        typedef typename Iterator::pointer pointer;
+        typedef typename Iterator::reference reference;
+        typedef typename Iterator::iterator_category iterator_category;
+};
+
+template <class T> struct iterator_traits<T *> {
+        typedef std::ptrdiff_t difference_type;
+        typedef T value_type;
+        typedef T *pointer;
+        typedef T &reference;
+        typedef std::random_access_iterator_tag iterator_category;
+};
+
+template <class T> struct iterator_traits<const T *> {
+        typedef std::ptrdiff_t difference_type;
+        typedef T value_type;
+        typedef const T *pointer;
+        typedef const T &reference;
+        typedef std::random_access_iterator_tag iterator_category;
+};
+} // namespace ft
+
+#endif /* ITERATOR_TRAITS_HPP */
