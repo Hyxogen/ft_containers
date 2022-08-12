d9ed7bdbda6931974f7062b12aa8b904666c246c
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-08-12 15:31:24 +0200

    remove stray 'n'

--- test/sequences/vector/cons/cons.iter.iter.alloc.cpp
+++ test/sequences/vector/cons/cons.iter.iter.alloc.cpp
@@ -11,7 +11,7 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-n//
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <allocators.hpp>
