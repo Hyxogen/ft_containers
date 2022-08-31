fae62563c54c29bf979d05bc4979b82113105b51
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-08-31 18:26:49 +0200

    format code

--- test/sequences/vector/mod/mod.erase.iter.iter.cpp
+++ test/sequences/vector/mod/mod.erase.iter.iter.cpp
@@ -14,10 +14,10 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#include <vector.hpp>
 #include <cassert>
 #include <classes.hpp>
 #include <string>
+#include <vector.hpp>
 
 int main() {
         {
