f2f3648ff52bcc15bc6dd9f6c287deb10415e686
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-10-30 09:39:44 +0100

    add benchmarks for vector insert

---
+++ benchmark/sequences/vector/mod/mod.insert.iter.size.value.cpp
@@ -0,0 +1,8 @@
+#include <benchmark.hpp>
+#include <cstdlib>
+
+int main() {
+        BENCHMARK(vector<int> vec(10000, 42);
+                  vec.insert(vec.begin() + rand() % 10000, rand() % 100, 42));
+        return 0;
+}
---
+++ benchmark/sequences/vector/mod/mod.insert.iter.value.cpp
@@ -0,0 +1,8 @@
+#include <benchmark.hpp>
+#include <cstdlib>
+
+int main() {
+        BENCHMARK(vector<int> vec(10000, 42);
+                  vec.insert(vec.begin() + rand() % 10000, 99));
+        return 0;
+}
