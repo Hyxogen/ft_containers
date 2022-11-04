18ce8abb858410ffe6254d8f7d8bb3520bac7c34
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-11-04 08:57:56 +0100

    add benchmarks

---
+++ benchmark/sequences/vector/cap/cap.resize.cpp
@@ -0,0 +1,8 @@
+#include <benchmark.hpp>
+#include <cstdlib>
+
+int main() {
+        vector<int> vec(10000);
+        BENCHMARK(vec.resize(rand() % 10000); DO_NOT_OPTIMIZE(vec));
+        return 0;
+}
---
+++ benchmark/sequences/vector/mod/mod.pop_back.cpp
@@ -0,0 +1,7 @@
+#include <benchmark.hpp>
+
+int main() {
+        BENCHMARK(vector<int> vec(10000); vec.pop_back();
+                  DO_NOT_OPTIMIZE(vec));
+        return 0;
+}
