a8d9d8d7e7aca098730ae84a9ef1f408affe1a95
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-11-04 10:27:43 +0100

    add brackets

--- include/__algorithm/lexicographical_compare.hpp
+++ include/__algorithm/lexicographical_compare.hpp
@@ -24,10 +24,12 @@ bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
                              InputIt2 last2) {
         for (; (first1 != last1) && (first2 != last2);
              ++first1, (void)++first2) {
-                if (*first1 < *first2)
+                if (*first1 < *first2) {
                         return true;
-                if (*first2 < *first1)
+                }
+                if (*first2 < *first1) {
                         return false;
+                }
         }
         return (first1 == last1) && (first2 != last2);
 }
@@ -37,10 +39,12 @@ bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
                              InputIt2 last2, Compare comp) {
         for (; (first1 != last1) && (first2 != last2);
              ++first1, (void)++first2) {
-                if (comp(*first1, *first2))
+                if (comp(*first1, *first2)) {
                         return true;
-                if (comp(*first2, *first1))
+                }
+                if (comp(*first2, *first1)) {
                         return false;
+                }
         }
         return (first1 == last1) && (first2 != last2);
 }
