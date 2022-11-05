20a168411aa691bd9e9a8fe2331b6ffd707df3e2
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-11-05 07:35:31 +0100

    decrease benchmark count

--- benchmark/sequences/vector/mod/mod.insert.iter.iiter.iiter.cpp
+++ benchmark/sequences/vector/mod/mod.insert.iter.iiter.iiter.cpp
@@ -1,8 +1,8 @@
 #include <benchmark.hpp>
 #include <cstdlib>
+#include <iterator>
 #include <sstream>
 #include <string>
-#include <iterator>
 
 int main() {
         const std::string str(
@@ -15,10 +15,10 @@ Senectus et netus et malesuada fames. Odio eu feugiat pretium nibh ipsum. Aliqua
 Cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Ullamcorper sit amet risus nullam. Turpis in eu mi bibendum neque egestas congue. Elit eget gravida cum sociis. Eu turpis egestas pretium aenean. Suspendisse in est ante in. Malesuada fames ac turpis egestas sed tempus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Rhoncus aenean vel elit scelerisque mauris. Ipsum suspendisse ultrices gravida dictum fusce ut. Nullam ac tortor vitae purus. Vel quam elementum pulvinar etiam. Cursus metus aliquam eleifend mi. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Id cursus metus aliquam eleifend mi in nulla posuere. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. Quis risus sed vulputate odio ut enim. Blandit massa enim nec dui nunc mattis enim ut tellus. Molestie nunc non blandit massa. Dignissim cras tincidunt lobortis feugiat.\
 \
 Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Amet commodo nulla facilisi nullam vehicula. Nam libero justo laoreet sit amet cursus sit. Rhoncus est pellentesque elit ullamcorper dignissim. Arcu odio ut sem nulla. Ac felis donec et odio pellentesque diam volutpat commodo sed. Massa sapien faucibus et molestie ac feugiat sed. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. Leo urna molestie at elementum. Amet tellus cras adipiscing enim eu turpis egestas. Varius duis at consectetur lorem. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Feugiat vivamus at augue eget arcu dictum. Mollis aliquam ut porttitor leo a diam sollicitudin. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo.");
-        BENCHMARK(std::stringstream stream(str);
-                  vector<std::string> vec(1000);
-                  vec.insert(vec.begin() + rand() % 1000,
-                             std::istream_iterator<std::string>(stream),
-                             std::istream_iterator<std::string>());
-                  DO_NOT_OPTIMIZE(vec));
+        BENCHMARK_N(std::stringstream stream(str);
+                    vector<std::string> vec(1000);
+                    vec.insert(vec.begin() + rand() % 1000,
+                               std::istream_iterator<std::string>(stream),
+                               std::istream_iterator<std::string>());
+                    DO_NOT_OPTIMIZE(vec), 100);
 }
