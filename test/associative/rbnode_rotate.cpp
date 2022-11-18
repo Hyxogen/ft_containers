0fb6b80f0405b780204eb393202ea9ecdefe40f5
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-11-18 10:21:39 +0100

    fix rotate tests

--- test/associative/rbnode_rotate.cpp
+++ test/associative/rbnode_rotate.cpp
@@ -1,13 +1,16 @@
 #include <__tree/red_black_tree.hpp>
 #include <cassert>
 #include <iostream>
-#include <stdexcept>
 #include <memory>
+#include <stdexcept>
 
 int main() {
-        using namespace ft::detail;
         {
-                rbtree<int, int, std::allocator<rbnode<int> > > tree;
+                typedef ft::detail::rbtree<
+                    int, int, std::allocator<ft::detail::rbnode<int> > >
+                    rbtree;
+
+                rbtree tree;
 
                 tree.insert(1);
                 tree.print();
@@ -16,19 +19,19 @@ int main() {
                 tree.insert(2);
                 tree.print();
                 assert(tree.is_valid());
-                
+
                 tree.rotate_left(tree.root());
                 assert(tree.is_bst());
-                
+
                 tree.rotate_right(tree.root());
                 assert(tree.is_valid());
 
-                rbtree<int, int, std::allocator<rbnode<int> > > ctree;
+                rbtree ctree;
 
-                tree.insert(1);
-                tree.insert(0);
-                tree.insert(2);
-                
-                assert(tree == ctree);
+                ctree.insert(1);
+                ctree.insert(0);
+                ctree.insert(2);
+
+                rbtree::assert_equal(tree, ctree);
         }
 }
