2f0c2c74920f68a5cc70b452a32bdcaa024a8f75
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-12-20 14:52:25 +0100

    add (const_)reference member type to stack

--- include/stack.hpp
+++ include/stack.hpp
@@ -9,6 +9,8 @@ template <class T, class Container = ft::vector<T> > class stack {
       public:
         typedef typename Container::value_type value_type;
         typedef typename Container::size_type size_type;
+	typedef typename Container::reference reference;
+	typedef typename Container::const_reference const_reference;
         typedef Container container_type;
 
       protected:
