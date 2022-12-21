550e42092da476b345a5753715440effa61b31fd
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-12-21 10:08:34 +0100

    add test for stateful allocator

--- test/sequences/vector/cons/cons.alloc.cpp
+++ test/sequences/vector/cons/cons.alloc.cpp
@@ -4,11 +4,19 @@
 
 int main() {
 	{
-		test::allocator_wrapper<int> alloc;
-		std::size_t count = test::calls["copy"];
-		ft::vector<int, test::allocator_wrapper<int> > vec(alloc);
-		assert(vec.empty());
-		assert((test::calls["copy"] - count) == 1);
-	}
+            test::allocator_wrapper<int> alloc;
+            std::size_t count = test::calls["copy"];
+            ft::vector<int, test::allocator_wrapper<int> > vec(alloc);
+            assert(vec.empty());
+            assert((test::calls["copy"] - count) == 1);
+        }
+        {
+            typedef int V;
+            typedef test::stateful_allocator<int, V> A;
+            typedef ft::vector<V, A> M;
+            M m(A(11));
+
+            assert(m.get_allocator() == A(11));
+        }
 	return 0;
 }
