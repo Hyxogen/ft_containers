59fd5a0753824ddae4a43de32786be2b4ca74f51
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-12-21 10:08:52 +0100

    add test for operator== rbtree_iterator

---
+++ test/associative/rbtree/iter/iter.op.eq.cpp
@@ -0,0 +1,24 @@
+#include <__tree/red_black_tree.hpp>
+#include <cassert>
+#include <functional>
+#include <memory>
+
+int main() {
+    using namespace ft::detail;
+    typedef int V;
+    typedef rbtree<V, V, use_self<V>, std::less<V>, std::allocator<rbnode<V> > >
+        M;
+    typedef M::iterator I;
+    {
+        I it1, it2;
+
+        assert(it1 == it2);
+    }
+    {
+        M m;
+
+        I it1 = m.begin(), it2 = m.begin();
+
+        assert(it1 == it2);
+    }
+}
